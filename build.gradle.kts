46b8bc3f6aa76628ff0a6aacd4a41ca80cb407cc
Sahil Bajaj <sahilbajaj.nc@gmail.com>
2021-11-24 17:57:30 +0530

    Upgrade korim to 2.2.2-IV2

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,17 @@ val buildExtraGradleFile = File(rootDir, "build.extra.gradle.kts")
 if (buildExtraGradleFile.exists()) {
     apply(from = buildExtraGradleFile)
 }
+allprojects {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.jetbrains.space/invideo/p/mobile/korgelibs")
+
+            val spaceUsername = project.properties["space.username"] as String?
+            val spacePassword = project.properties["space.password"] as String?
+            credentials {
+                username = spaceUsername
+                password = spacePassword
+            }
+        }
+    }
+}
