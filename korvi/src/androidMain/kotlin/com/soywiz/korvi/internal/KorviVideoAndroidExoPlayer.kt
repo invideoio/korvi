33dda2d8d74a656e2726fe61090405e107c787d2
Dhruv Gupta <dhruv2295@gmail.com>
2022-02-16 13:43:03 +0530

    Surface Creation Optimistation (#8)
    * Surface optimisation rebased
    
    * continuous rendering with conditional texture updates
    
    Co-authored-by: Sahil Bajaj <sahilbajaj.nc@gmail.com>

--- korvi/src/androidMain/kotlin/com/soywiz/korvi/internal/KorviVideoAndroidExoPlayer.kt
+++ korvi/src/androidMain/kotlin/com/soywiz/korvi/internal/KorviVideoAndroidExoPlayer.kt
@@ -27,6 +27,11 @@ class AndroidKorviVideoAndroidExoPlayer(context: Context) : KorviVideo() {
     private var currentSeek = -1L
     private var pendingSeek = -1L
 
+    private var isPrepared = false
+    private var hasSurface = false
+    private lateinit var info: SurfaceTextureInfo
+
+
     fun setVolume(volume: Float) {
         player.volume = volume
     }
@@ -37,7 +42,8 @@ class AndroidKorviVideoAndroidExoPlayer(context: Context) : KorviVideo() {
     }
 
     fun setMedia(mediaItem: MediaItem) {
-        player.clearVideoSurface()
+        isPrepared = false
+//        player.clearVideoSurface()
         player.setMediaItem(mediaItem)
     }
 
@@ -49,10 +55,16 @@ class AndroidKorviVideoAndroidExoPlayer(context: Context) : KorviVideo() {
     private var frameAvailable = 0
 
     override fun prepare() {
-        val info = SurfaceNativeImage.createSurfacePair()
+        if (isPrepared) return
+        isPrepared = true
+
+        if (!hasSurface) {
+            info = SurfaceNativeImage.createSurfacePair()
+            hasSurface = true
+        }
         // println("SET SURFACE")
         info.texture.setOnFrameAvailableListener {
-            // println("frame available: $frameAvailable")
+//             println("frame available: $frameAvailable for duration ${player.duration}")
             frameAvailable++
         }
 
@@ -96,22 +108,22 @@ class AndroidKorviVideoAndroidExoPlayer(context: Context) : KorviVideo() {
     private val matrix = Matrix3D()
 
     override fun render() {
-        if (lastUpdatedFrame == frameAvailable) return
         try {
-//            println("AndroidKorviVideoAndroidExoPlayer.render! $frameAvailable")
-            val surfaceTexture = nativeImage.surfaceTexture
-            lastUpdatedFrame = frameAvailable
-            surfaceTexture.updateTexImage()
-            surfaceTexture.getTransformMatrix(transformMat)
+            if (lastUpdatedFrame != frameAvailable) {
+                val surfaceTexture = nativeImage.surfaceTexture
+                lastUpdatedFrame = frameAvailable
+                surfaceTexture.updateTexImage()
+                surfaceTexture.getTransformMatrix(transformMat)
 
-            matrix.setColumns4x4(transformMat, 0)
-            matrix.translate(0.0, +0.5, 0.0)
-            matrix.scale(1.0, -1.0, 1.0)
-            matrix.translate(0.0, -0.5, 0.0)
-            matrix.copyToFloat4x4(transformMat, MajorOrder.COLUMN)
+                matrix.setColumns4x4(transformMat, 0)
+                matrix.translate(0.0, +0.5, 0.0)
+                matrix.scale(1.0, -1.0, 1.0)
+                matrix.translate(0.0, -0.5, 0.0)
+                matrix.copyToFloat4x4(transformMat, MajorOrder.COLUMN)
 
-            nativeImage.transformMat.setColumns4x4(transformMat, 0)
-            lastTimeSpan = surfaceTexture.timestamp.toDouble().nanoseconds.hr
+                nativeImage.transformMat.setColumns4x4(transformMat, 0)
+                lastTimeSpan = surfaceTexture.timestamp.toDouble().nanoseconds.hr
+            }
             onVideoFrame(Frame(nativeImage, lastTimeSpan, frameRate.timeSpan.hr))
         } catch (e: Exception) {
             System.err.println(e.message)
@@ -155,16 +167,15 @@ class AndroidKorviVideoAndroidExoPlayer(context: Context) : KorviVideo() {
     }
 
     override suspend fun seek(time: HRTimeSpan) {
-        lastTimeSpan = time
+        val time = time.millisecondsInt.coerceAtLeast(0).toLong()
+        lastTimeSpan = HRTimeSpan.fromMilliseconds(time.toInt())
         withContext(Dispatchers.Main) {
-            //Todo seek through multiple media files
-//            player.seekTo(windowIndex, seekPos.toLong())
             if (currentSeek == -1L) {
-                currentSeek = time.millisecondsInt.toLong()
+                currentSeek = time
                 pendingSeek = -1L
                 player.seekTo(currentSeek)
             } else {
-                pendingSeek = time.millisecondsInt.toLong()
+                pendingSeek = time
             }
         }
     }
